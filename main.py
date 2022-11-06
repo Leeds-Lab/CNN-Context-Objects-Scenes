bc05139ff682c0897584f8e4591ba447c7fd92a3
Eric <Eroginek@gmail.com>
2022-11-06 11:01:38 -0500

    short flag options added along with help information for each argument

--- main.py
+++ main.py
@@ -25,35 +25,35 @@ all_args = argparse.ArgumentParser(description="Selects the CNN models and analy
 # Default arguments for analyses used in Aminoff et al. (2022) 
 # Investigate context/category responsiveness in a convolutional neural network by using Pearson's Correlation Coefficient
 # HOG and pixel similarity analysis of the image dataset (needs to be set to 1)
-all_args.add_argument("-run_net_responses", "--run_net_responses", default=1)
-all_args.add_argument("-run_compute_ratios", "--run_compute_ratios", default=1)
-all_args.add_argument("-pearson_charts", "--pearson_charts", default=1)
-all_args.add_argument("-pearson", '--pearson', default=1)
-all_args.add_argument("-confounds", '--confounds', default=1) # different data probably won't have confounds - change to False
-all_args.add_argument("-hog_pixel_similarity", "--hog_pixel_similarity", default=0)
-all_args.add_argument("-vgg16_contexts", "--vgg16_contexts", default=1) # produces contexts for vgg16 to compare with behavioral data in Aminoff et al. 2022
-all_args.add_argument("-ttests", "--ttests", default=1) # run t-test analyses in the manner done by Aminoff et al. 2022
+all_args.add_argument("-n", "--net_responses", default=1, help="extract cnn model network responses to image data for each convolution layer")
+all_args.add_argument("-c", "--compute_ratios", default=1, help="calculate context and category ratios for each cnn model network using matrix data created from --net_responses")
+all_args.add_argument("-pch", "--pearson_charts", default=1, help="create context and category line graphs using ratio pearson correlation information obtained from --compute_ratios")
+all_args.add_argument("-p", '--pearson', default=1, help="use pearson's correlation for analyzing --net_responses")
+all_args.add_argument("-cfs", '--confounds', default=1, help="use pairwise confound matrix data containing boolean values along the diagonal and other potentially confounding context/categories to remove values that might affect output results") # different data probably won't have confounds - change to False
+all_args.add_argument("-hps", "--hog_pixel_similarity", default=0, help="analyze the visual structure of the dataset to determine if there is visual similarity among the objects used")
+all_args.add_argument("-v16con", "--vgg16_contexts", default=1, help="singles out vgg16 contextual information for direct comparisions with contextual data obtained from human behavioral assessments") # produces contexts for vgg16 to compare with behavioral data in Aminoff et al. 2022
+all_args.add_argument("-tt", "--ttests", default=1, help="perform t-tests using the max context/category ratio extracted from each cnn model for each context/category data item used. Options include t-tests on all models, models trained on ImageNet vs Places365, and shallow vs deep networks as examples") # run t-test analyses in the manner done by Aminoff et al. 2022
 
 # models
-all_args.add_argument("-all_models", "--all_models", default=0)
-all_args.add_argument("-alexnet", '--alexnet', default=0)
-all_args.add_argument("-alexnet_places365", "--alexnet_places365", default=0)
-all_args.add_argument("-vgg16", "--vgg16", default=0)
-all_args.add_argument("-vgg19", "--vgg19", default=0)
-all_args.add_argument("-resnet18", '--resnet18', default=0)
-all_args.add_argument("-resnet18_places365", "--resnet18_places365", default=0)
-all_args.add_argument("-resnet50", '--resnet50', default=0)
-all_args.add_argument("-resnet50_places365", "--resnet50_places365", default=0)
-all_args.add_argument("-resnext50_32x4d", '--resnext50_32x4d', default=0)
-all_args.add_argument("-resnet101", '--resnet101', default=0)
-all_args.add_argument("-resnet152", '--resnet152', default=0)
-all_args.add_argument("-googlenet", '--googlenet', default=0)
-all_args.add_argument("-grcnn55", '--grcnn55', default=0)
+all_args.add_argument("-a", "--all_models", default=0, help='use all the models currently available in load_weights.py')
+all_args.add_argument("-anet", '--alexnet', default=0, help='AlexNet pretrained on ImageNet')
+all_args.add_argument("-anetp", "--alexnet_places365", default=0, help='AlexNet pretrained on Places365')
+all_args.add_argument("-v16", "--vgg16", default=0, help='VGG16 pretrained on ImageNet')
+all_args.add_argument("-v19", "--vgg19", default=0, help='VGG19 pretrained on ImageNet')
+all_args.add_argument("-r18", '--resnet18', default=0, help='ResNet18 pretrained on ImageNet')
+all_args.add_argument("-r18p", "--resnet18_places365", default=0, help='ResNet18 pretrained on Places365')
+all_args.add_argument("-r50", '--resnet50', default=0, help='ResNet50 pretrained on ImageNet')
+all_args.add_argument("-r50p", "--resnet50_places365", default=0, help='ResNet50 pretrained on Places365')
+all_args.add_argument("-rx50", '--resnext50_32x4d', default=0, help='ResNext50_32x4d pretrained on ImageNet')
+all_args.add_argument("-r101", '--resnet101', default=0, help='ResNet101 pretrained on ImageNet')
+all_args.add_argument("-r152", '--resnet152', default=0, help='ResNet152 pretrained on ImageNet')
+all_args.add_argument("-gnet", '--googlenet', default=0, help='GoogLeNet pretrained on ImageNet')
+all_args.add_argument("-g55", '--grcnn55', default=0, help='GRCNN-55 pretrained on ImageNet')
 
 # Other arguments include using additional networks and analyses
-all_args.add_argument("-h_cluster", '--h_cluster', default=0)
-all_args.add_argument("-m_MDS", '--m_MDS', default=0)
-all_args.add_argument("-m_TSNE", '--m_TSNE', default=0)
+all_args.add_argument("-hc", '--h_cluster', default=0, help='perform hierarchical cluster analysis for analyzing --net_responses')
+all_args.add_argument("-mds", '--m_MDS', default=0, help='perform manifold analysis MDS for analyzing --net_responses')
+all_args.add_argument("-tsne", '--m_TSNE', default=0, help='perform manifold analysis t-SNE for analyzing --net_responses')
 
 args = vars(all_args.parse_args())
 
@@ -93,12 +93,12 @@ if __name__ == "__main__":
         if os.path.exists(OUTPUT_MODELS_PATH) == False: os.mkdir(OUTPUT_MODELS_PATH)
 
         # Process and analyze particular neural network models
-        if int(args["run_net_responses"]) == 1:
+        if int(args["net_responses"]) == 1:
             CNN_Eval = Network_Evaluator(models_for_analysis, batch_analysis, DIRECTORIES_FOR_ANALYSIS, START_FILE_NUMBER, END_FILE_NUMBER)
             CNN_Eval.run_network_responses()
 
         # Compute ratio of in-category/out-category and in-context/out-context for Pearson's Correlation Matrices
-        if int(args["run_compute_ratios"]) == 1 and int(args["pearson"]) == 1:
+        if int(args["compute_ratios"]) == 1 and int(args["pearson"]) == 1:
             RATIO_FILENAME = "_pearson_ratios"
             Matrix_Eval = Matrix_Evaluator(models_for_analysis, PEARSON_PATH, RATIO_FILENAME, confounds) 
             Matrix_Eval.compute_ratios()
